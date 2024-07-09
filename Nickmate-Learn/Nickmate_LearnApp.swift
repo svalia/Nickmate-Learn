c69c4548955b2198e9f239c6afecd89bea761743
Valia Smolin <Valia@Smolin.Ru>
2024-07-09 23:35:18 +0300

    Initial commit

---
+++ Nickmate-Learn/Nickmate_LearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nickmate_LearnApp.swift
+//  Nickmate-Learn
+//
+//  Created by Valia Smolin on 09.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Nickmate_LearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
